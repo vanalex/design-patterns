1b76631d1fa64a186ccd0249370523123169aa85
alejandropajaronjorquera <a.pajaron.jorquera@dkbcodefactory.com>
2020-10-23 15:20:38 +0200

    add more examples on templates pattern and tests.

--- src/test/java/org/example/template/barista/TemplateTest.java
+++ src/test/java/org/example/template/barista/TemplateTest.java
@@ -17,4 +17,26 @@ public class TemplateTest {
         coffee.addCondiments();
         coffee.addCondiments();
     }
+
+    @Test
+    public void testBeverageWithHook(){
+        Tea tea = new Tea();
+        Coffee coffee = new Coffee();
+
+        System.out.println("\nMaking tea...");
+        tea.prepareRecipe();
+
+        System.out.println("\nMaking coffee...");
+        coffee.prepareRecipe();
+
+
+        TeaWithHook teaHook = new TeaWithHook();
+        CoffeeWithHook coffeeHook = new CoffeeWithHook();
+
+        System.out.println("\nMaking tea...");
+        teaHook.prepareRecipe();
+
+        System.out.println("\nMaking coffee...");
+        coffeeHook.prepareRecipe();
+    }
 }
